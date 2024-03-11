2d4ea3820689ec8e03eaf59d356a2e1ee67cc43d
Rafael <rafaelaugustodev@gmail.com>
2024-03-11 18:43:05 -0300

    feat: creating ProjectService

---
+++ src/main/java/com/rafaelaugusto/portfolio/services/ProjectService.java
@@ -0,0 +1,16 @@
+package com.rafaelaugusto.portfolio.services;
+
+import com.rafaelaugusto.portfolio.domain.entities.Project;
+import com.rafaelaugusto.portfolio.domain.enums.ProjectStatus;
+import com.rafaelaugusto.portfolio.dtos.ProjectDTO;
+import com.rafaelaugusto.portfolio.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+
+}
