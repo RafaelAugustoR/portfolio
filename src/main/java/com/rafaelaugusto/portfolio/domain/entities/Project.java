4dcc5ca5cd18aec5e728502954dfb6bdfeed3cd6
Rafael <rafaelaugustodev@gmail.com>
2024-03-10 22:52:38 -0300

    feat: Creating entity Project

---
+++ src/main/java/com/rafaelaugusto/portfolio/domain/entities/Project.java
@@ -0,0 +1,26 @@
+package com.rafaelaugusto.portfolio.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String imgUrl;
+    private String stacks;
+
+}
