a9c1d3e13326a08942f118e1a4168d11b44acd15
Rafael <rafaelaugustodev@gmail.com>
2024-03-10 22:54:13 -0300

    feat: Creating DTO

---
+++ src/main/java/com/rafaelaugusto/portfolio/dtos/ProjectDTO.java
@@ -0,0 +1,24 @@
+package com.rafaelaugusto.portfolio.dtos;
+
+import com.rafaelaugusto.portfolio.domain.entities.Project;
+import com.rafaelaugusto.portfolio.domain.enums.ProjectStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectDTO {
+
+    private String name;
+    private String description;
+    private String imgUrl;
+    private String stacks;
+    private ProjectStatus status;
+
+    public ProjectDTO(Project entity){
+        BeanUtils.copyProperties(entity, this);
+    }
+}
