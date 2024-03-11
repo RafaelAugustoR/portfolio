708c1589cba62553eeff94d6830aa057c469e2b0
Rafael <rafaelaugustodev@gmail.com>
2024-03-11 18:44:28 -0300

    feat: creating a method 'create' to save the information in the database

--- src/main/java/com/rafaelaugusto/portfolio/services/ProjectService.java
+++ src/main/java/com/rafaelaugusto/portfolio/services/ProjectService.java
@@ -12,5 +12,24 @@ import java.util.List;
 @Service
 public class ProjectService {
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public void create(ProjectDTO request){
+
+        Project entityToSave = Project.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .stacks(request.getStacks())
+                .imgUrl(request.getImgUrl())
+                .status(request.getStatus())
+                .build();
+
+
+        projectRepository.save(entityToSave);
+
+    }
+
+
 
 }
